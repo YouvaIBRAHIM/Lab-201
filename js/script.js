8519025ad2185e01ccbf5a5f16b45481836db7e9
Youva <youva.ibrahim@gmail.com>
2021-03-31 17:01:07 +0200

    Lab201 burger menu done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,60 @@
 import firebaseConfig from './firebaseConfig.js';
 import onToggleLogin from "./login.js";
 import {onMouseMove, onMouseClick} from "./cursor.js";
-import "./contact.js"
-import "./tours.js"
+import "./contact.js";
+import "./tours.js";
+
 const login = document.querySelector('.login');
 if (login) {
    login.addEventListener('click', onToggleLogin); 
 }
-
+const menuToggle = document.querySelector('.menuToggle');
+const nav = document.querySelector('header nav');
+const navContainer = document.querySelector('header .contain');
+const mainBody = document.querySelector('main');
+const navBarLi = document.querySelectorAll('header .contain nav .nav-bar li');
+const shopBtn = document.querySelector('.shop');
+let isBurgerMenuDisplayed = false;
+function onToggleMenu() {
+   menuToggle.classList.toggle('expanded');
+   navContainer.classList.toggle('expanded');
+   nav.classList.toggle('expanded');
+   for (let i = 0; i < navBarLi.length; i++) {
+      navBarLi[i].classList.toggle('expanded');
+   }
+   shopBtn.classList.toggle('expanded');
+}
+menuToggle.addEventListener('click', (event) => {
+   isBurgerMenuDisplayed = !isBurgerMenuDisplayed;
+   onToggleMenu();
+})
+mainBody.addEventListener('click', (event) => {
+   if (isBurgerMenuDisplayed === true) {
+      onToggleMenu();
+      isBurgerMenuDisplayed = !isBurgerMenuDisplayed;
+   }
+   
+})
 document.addEventListener('mousemove', onMouseMove)
 document.addEventListener('click', onMouseClick)
 const videoContainer = document.querySelector('.videos .container');
+
 document.addEventListener('scroll', (event) => {
-   if (scrollY <= 650) {
+   if (window.innerWidth >= 550) {
+      if (scrollY <= 650) {
       videoContainer.style.top = '650px';
       videoContainer.style.transform = "scale(0.6)";
+      }
+      if (scrollY <= 936 && scrollY >= 650) {
+         videoContainer.style.position = 'absolute';
+         videoContainer.style.top = (scrollY + 150)+ 'px';
+         videoContainer.style.transform = "scale(1)";
+      }
+      if (scrollY >= 936) {
+         videoContainer.style.top = 'auto';
+         videoContainer.style.position = 'relative';
+      }
    }
-   if (scrollY <= 936 && scrollY >= 650) {
-      videoContainer.style.position = 'absolute';
-      videoContainer.style.top = (scrollY + 150)+ 'px';
-      videoContainer.style.transform = "scale(1)";
-   }
-   if (scrollY >= 936) {
-      videoContainer.style.top = 'auto';
-      videoContainer.style.position = 'relative';
-   }
+   
    
 })
--- js/tours.js
+++ js/tours.js
@@ -1,20 +1,30 @@
 let isDisplayAllUsaDates = false;
+let isDisplayAllEuropeDates = false;
 const usaTour = document.querySelector('.usaTour .dates');
-const moreLessDates = document.querySelector('.usaTour .more-less-dates');
-moreLessDates.addEventListener('click', () => {
+const europeTour = document.querySelector('.europeTour .dates');
+const UsaMoreLessDates = document.querySelector('.usaTour .more-less-dates');
+const europeMoreLessDates = document.querySelector('.europeTour .more-less-dates');
+UsaMoreLessDates.addEventListener('click', () => {
     isDisplayAllUsaDates = !isDisplayAllUsaDates;
     usaTourDisplay();
 })
+europeMoreLessDates.addEventListener('click', () => {
+    isDisplayAllEuropeDates = !isDisplayAllEuropeDates;
+    europeTourDisplay();
+})
 
-function getDateTour() {
+function getDateTour(dates, index) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let dateTour = new Date(dates[index].date).toLocaleDateString('fr-FR', options).toString().split(" ");
+    dateTour = `${dateTour[1]} ${dateTour[2]}`;
     return `<div class="date">
-    <h5 class="day-month-year">5 Jan 2022</h4>
-    <h2 class="city">Chicago, EU</h2>
-    <h3 class="place">Stadium De Vinci</h3>
+    <h5 class="day-month-year">${dateTour}</h4>
+    <h2 class="city">${dates[index].city}</h2>
+    <h3 class="place">${dates[index].place}</h3>
     <div class="customButton">
         <div class="wrapper">
             <div class="link_wrapper">
-            <a href="#"><span>RÉSERVER</span></a>
+            <a href="${dates[index].link}"><span>RÉSERVER</span></a>
             <div class="icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
                 <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
@@ -26,20 +36,59 @@ function getDateTour() {
 </div>`;
 }
 
-function usaTourDisplay() {
-    if (isDisplayAllUsaDates === false) {
-        usaTour.innerHTML = "";
-        for (let i = 0; i < 4; i++) {
-            let date = getDateTour();
-            usaTour.innerHTML += date;
-        }
-    }else{
-        usaTour.innerHTML = "";
-        for (let i = 0; i < 8; i++) {
-            let date = getDateTour();
-            usaTour.innerHTML += date;
-        }
-    }
-    
+async function usaTourDisplay() {
+    let dates;
+    let usaDates = [];
+    let firebaseDb = await firebase.database().ref();
+        firebaseDb.child('dates').on('value', snap => {
+            dates = Object.values(snap.val());
+            for (let i = 0; i < dates.length; i++) {
+                if (dates[i].continentCountry === "USA") {
+                    usaDates.push(dates[i]);
+                }
+            }
+            if (isDisplayAllUsaDates === false) {
+                usaTour.innerHTML = "";
+                for (let i = 0; i < 4; i++) {
+                    let date = getDateTour(usaDates, i);
+                    usaTour.innerHTML += date;
+                }
+            }else{
+                usaTour.innerHTML = "";
+                for (let i = 0; i < usaDates.length; i++) {
+                    let date = getDateTour(usaDates, i);
+                    usaTour.innerHTML += date;
+                }
+            }
+        })
 }
+async function europeTourDisplay() {
+    let dates;
+    let europeDates = [];
+    let firebaseDb = await firebase.database().ref();
+        firebaseDb.child('dates').on('value', snap => {
+            dates = Object.values(snap.val());
+            
+            for (let i = 0; i < dates.length; i++) {
+                if (dates[i].continentCountry === "Europe") {
+                    europeDates.push(dates[i]);
+                }
+            }
+            console.log(europeDates)
+            if (isDisplayAllEuropeDates === false) {
+                europeTour.innerHTML = "";
+                for (let i = 0; i < 4; i++) {
+                    let date = getDateTour(europeDates, i);
+                    europeTour.innerHTML += date;
+                }
+            }else{
+                europeTour.innerHTML = "";
+                for (let i = 0; i < europeDates.length; i++) {
+                    let date = getDateTour(europeDates, i);
+                    europeTour.innerHTML += date;
+                }
+            }
+        })
+}
+europeTourDisplay();
 usaTourDisplay();
