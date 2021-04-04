5c67f47efd17b63f5c569c302586c534404cbeff
Youva <youva.ibrahim@gmail.com>
2021-04-04 12:41:41 +0200

    Lab201 dates soldOut added

--- js/tours.js
+++ js/tours.js
@@ -31,25 +31,38 @@ function getDateTour(dates, index) {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let dateTour = new Date(dates[index].date).toLocaleDateString('fr-FR', options).toString().split(" ");
     dateTour = `${dateTour[1]} ${dateTour[2]}`;
-    return `<div class="date">
+    let policeColor = "";
+    let isAvailable = dates[index].isAvailable;
+    let btnToDisplay = "";
+    if (isAvailable === true) {
+        policeColor = "black";
+        btnToDisplay = `<div class="customButton">
+                            <div class="wrapper">
+                                <div class="link_wrapper">
+                                <a href="${dates[index].link}"><span>RÉSERVER</span></a>
+                                <div class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
+                                    <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
+                                    </svg>
+                                </div>
+                                </div>
+                            </div>
+                        </div>`;
+    }else{
+        policeColor = "grey";
+        btnToDisplay = `<div class="soldOut">
+                            <span>COMPLET</span>
+                        </div>`;
+    }
+
+    return `<div style="color:${policeColor};" class="date">
     <h5 class="day-month-year">${dateTour}</h4>
     <h2 class="city">${dates[index].city}, ${dates[index].country}</h2>
     <h3 class="place">${dates[index].place}</h3>
-    <div class="customButton">
-        <div class="wrapper">
-            <div class="link_wrapper">
-            <a href="${dates[index].link}"><span>RÉSERVER</span></a>
-            <div class="icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
-                <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
-                </svg>
-            </div>
-            </div>
-        </div>
-    </div>
-    <a class="mobileBookBtn" href="${dates[index].link}" target="_blank" rel="noopener noreferrer">
-        <span>Réserver</span>
-    </a>
+    ${btnToDisplay}
+    
+    
+    
 </div>`;
 }
 
